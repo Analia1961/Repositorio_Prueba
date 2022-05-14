600d7fc2a205d1abbb2aa4525f0ce29a7f35a62d
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-14 12:53:44 -0300

    ingreso-menu pruebo la estructura del menú y los submenús

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,65 @@ Ya es hora de comenzar a organizar las tareas
 
 document.getElementById("app").innerHTML = `
 <h1>ORGANIZADOR DE TAREAS </h1>
+
 <h2>CLASE 1 </h2>
 <div>
-TAREA 1<br />
-  info about Parcel <br />
-  <a href="#URL" target="_blank" rel="noopener noreferrer">here</a>.
+TAREA (título1)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+</div>
 
-</div>`;
+<h2>CLASE 2 </h2>
+<div>
+TAREA (título1)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+<br/><br/>
+  TAREA (título2)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+  <br/><br/>
+  TAREA (título3)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+  </div>
+
+<h2>CLASE 3 </h2>
+<div>
+TAREA (título1)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+  <br/><br/>
+  TAREA (título2)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+</div>
+
+<h2>CLASE 4 </h2>
+<div>
+TAREA (título1)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+</div>
+
+<h2>CLASE 5 </h2>
+<div>
+TAREA (título1)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+</div>
+
+<h2>CLASE 6 </h2>
+<div>
+TAREA (título1)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+</div>
+
+<h2>CLASE 7 </h2>
+<div>
+TAREA (título1)<br />
+  información <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+</div>
+`;
