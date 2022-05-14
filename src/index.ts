7eeda600199e7fdfebe48ee986bd2726a4584e86
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-14 11:36:24 -0300

    comienzan_las_pruebas perdida en El Perdido

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+<h1>Hola Analía!</h1>
+Ya es hora de comenzar a organizar las tareas
 </div>`;
