5d949f318cb8016d14d5e91c077f480ae22c5c89
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-14 20:09:48 -0300

    agrego-enlaces completo menu prueba

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,21 @@ Ya es hora de comenzar a organizar las tareas
 </div>`;*/
 
 document.getElementById("app").innerHTML = `
+<a href="#URL" target="_blank" rel="noopener noreferrer">
 <h1>ORGANIZADOR DE TAREAS </h1>
-
+</a>
+<div>
 <h2>CLASE 1 </h2>
-<div>
-TAREA (título1)<br />
-  información <br />
-  <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
+<h4>Introducción al curso<br />
+Lunes 18 de abril de 2022 </h4>
+TAREA 1 (Ejercicio de clase)<br />
+  <a href="https://codesandbox.io/s/github/Analia1961/ejercicio1.ts_clase1-Lunes18abril/tree/main?file=/src/ejercicio1.ts" target="_blank" rel="noopener noreferrer">Crear un posteo en Facebook</a>.
 </div>
-
-<h2>CLASE 2 </h2>
+<br/>
 <div>
+<h2>CLASE 2 </h2>
+<h4> Algoritmos Secuenciales<br/>
+Viernes 22 de abril de 2022</h4>
 TAREA (título1)<br />
   información <br />
   <a href="#URL" target="_blank" rel="noopener noreferrer">enlace</a>.
