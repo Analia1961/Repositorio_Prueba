56181d9c4a9a66d6d67b772264bd674d0c0fafa6
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-14 12:38:57 -0300

    pruebo_Menus primeras pruebas para organizar las tareas y enlazarlas.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
+/*document.getElementById("app").innerHTML = `
 <h1>Hola Analía!</h1>
 Ya es hora de comenzar a organizar las tareas
+</div>`;*/
+
+document.getElementById("app").innerHTML = `
+<h1>ORGANIZADOR DE TAREAS </h1>
+<h2>CLASE 1 </h2>
+<div>
+TAREA 1<br />
+  info about Parcel <br />
+  <a href="#URL" target="_blank" rel="noopener noreferrer">here</a>.
+
 </div>`;
